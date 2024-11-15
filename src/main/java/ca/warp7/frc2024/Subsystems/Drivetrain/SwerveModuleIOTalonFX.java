16a288574f5870cdca9f6480e581acfdbef7ee5d
Elliot Tam <eteaisme@gmail.com>
2024-11-14 23:07:49 -0500

    Removed Errors

--- src/main/java/ca/warp7/frc2024/Subsystems/Drivetrain/SwerveModuleIOTalonFX.java
+++
@@ -1,9 +0,0 @@
-package ca.warp7.frc2024.Subsystems.Drivetrain;
-
-<<<<<<< HEAD
-public class ModuleIOTalonFX implements ModuleIO{
-=======
-public class SwerveModuleIOTalonFX implements SwerveModuleIO{
->>>>>>> 1c78f58003e65d261e8ee6fcd0f8517ed962a644
-    
-}
